7473fed0af7429ef74d800f687b705e37697238a
ahmadromadhani007 <dan10kiy@gmail.com>
2022-10-20 17:14:20 +0700

    updated php succes

---
+++ testPhp.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Membuat File PHP Pertama</title>
+   </head>
+<body>
+<?php
+   echo "<h1>";
+   echo "Selamat datang di websiteku..";
+   echo "</h1>";
+?>
+</body>
+</html>
